8ec35adfc44b9773d1d1929334d712903e76b4b2
Pedro <ph.pfiorentin@gmail.com>
2021-11-12 22:10:33 +0100

    Converted project to python and added pytest tests

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def is_number(a):
+    return isinstance(a, (int, float))
+
+def validate_input(a, b):
+    if not is_number(a) or not is_number(b):
+        raise ValueError('Input values must be numbers.')
+
+def sum(a, b):
+    validate_input(a, b)
+    return a + b
+
+def subtract(a, b):
+    validate_input(a, b)
+    return a - b 
+
+def mult(a, b):
+    validate_input(a, b)
+    return a * b
+
+def div(a, b):
+    validate_input(a, b)
+    if b == 0:
+        raise ValueError('Can\'t divide by zero.')
+    return a/b 
+
+#div(10, 'a')
---
+++ test_calculator.py
@@ -0,0 +1,52 @@
+import pytest
+import calculator as calc
+
+INVALID_INPUT_MESSAGE = "Input values must be numbers."
+
+def test_calculator_sum():
+    assert calc.sum(65, 355) == 420
+
+def test_calculator_sum_float():
+    assert calc.sum(3.9, 4.2) == pytest.approx(8.1)   
+
+def test_calculator_sum_string_error():
+    with pytest.raises(ValueError) as e_info:
+        calc.sum(3.9, 'abc')
+    e_message = str(e_info.value)
+    assert INVALID_INPUT_MESSAGE in e_message
+
+def test_calculator_subtraction():
+    assert calc.subtract(23, 40) == -17
+
+def test_calculator_subtraction_float():
+    assert calc.subtract(3.9, -4.2) == pytest.approx(8.1)   
+
+def test_calculator_subtraction_string_error():
+    with pytest.raises(ValueError) as e_info:
+        calc.subtract(3.9, 'abc')
+    e_message = str(e_info.value)
+    assert INVALID_INPUT_MESSAGE in e_message
+
+def test_calculator_multiplication():
+    assert calc.mult(3, 7) == 21
+
+def test_calculator_multiplication_float():
+    assert calc.mult(3, 7.4) == pytest.approx(22.2)
+
+def test_calculator_multiplication_string_error():
+    with pytest.raises(ValueError) as e_info:
+        calc.mult(3.9, 'abc')
+    e_message = str(e_info.value)
+    assert INVALID_INPUT_MESSAGE in e_message
+
+def test_calculator_division():
+    assert calc.div(10, 4) == pytest.approx(2.5)
+
+def test_calculator_division_float():
+    assert calc.div(26.25, 7.5) == pytest.approx(3.5)
+
+def test_calculator_division_string_error():
+    with pytest.raises(ValueError) as e_info:
+        calc.div(3.9, 'abc')
+    e_message = str(e_info.value)
+    assert INVALID_INPUT_MESSAGE in e_message
